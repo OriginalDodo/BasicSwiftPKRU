fff5dc2b551119788b5eea6e6247b88739565463
Com422Mac03 <com422mac03@Com422Mac03s-iMac.local>
2017-07-26 09:44:06 +0700

    Learn Array

---
+++ Aray.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การใช้ตัวแปร Aray
+// Imlicit and Assign Value
+let intArrayNumber1 = [11, 22, 33, 44, 55]
+
+// Explicit And Assign Value
+var intArrayNumber2 = [Int]()
+intArrayNumber2 = [66, 77, 88, 99]
+
+// String Type
+let strName1 = ["Doramon", "Nopita"]
+var strName2 = [String]()
+strName2 = ["Somchai", "Pichan"]
+
+// Double Type
+let douNumber1 = [1.2, 1.3, 1.4]
+var douNumber2 = [Double]()
+douNumber2 = [2.1, 2.2]
+
+
+
+
+
+
+
+
+
