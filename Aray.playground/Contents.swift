b798a0056547d0cb0029d4074926f336bd33f583
Com422Mac03 <com422mac03@Com422Mac03s-iMac.local>
2017-07-26 09:59:19 +0700

    Add Array

--- Aray.playground/Contents.swift
+++ Aray.playground/Contents.swift
@@ -4,14 +4,14 @@ import UIKit
 
 //การใช้ตัวแปร Aray
 // Imlicit and Assign Value
-let intArrayNumber1 = [11, 22, 33, 44, 55]
+var intArrayNumber1 = [11, 22, 33, 44, 55]
 
 // Explicit And Assign Value
 var intArrayNumber2 = [Int]()
 intArrayNumber2 = [66, 77, 88, 99]
 
 // String Type
-let strName1 = ["Doramon", "Nopita"]
+var strName1 = ["Doramon", "Nopita"]
 var strName2 = [String]()
 strName2 = ["Somchai", "Pichan"]
 
@@ -19,6 +19,16 @@ strName2 = ["Somchai", "Pichan"]
 let douNumber1 = [1.2, 1.3, 1.4]
 var douNumber2 = [Double]()
 douNumber2 = [2.1, 2.2]
+
+//การเพิ่มสมาชิก ให้กับตัวแปร Array
+print("intArrayNumber1 ==> \(intArrayNumber1)")
+intArrayNumber1.append(999)
+print("นี่คือ intArrayNumber1 ใหม่ ==> \(intArrayNumber1)")
+
+print("strName1 ==> \(strName1)")
+strName1.append("Dodo")
+print("New strName ==> \(strName1)")
+
 
 
 
